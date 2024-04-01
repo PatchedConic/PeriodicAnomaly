8bd2ce6cdd3b1f2ae4e0288de015811ecbc1b728
Patched Conic <Patched Conic @ github>
2024-04-01 01:48:27 -0700

    Basic math functionality and terminal usage implemented

---
+++ perly/__main__.py
@@ -0,0 +1,22 @@
+from .stack import Stack
+from .token import Token
+from .consts import *
+import re
+import sys
+
+def main(args):
+    stack = Stack()
+    for arg in args:
+        if re.search(r'^\d+(\.\d+)?$', arg):
+            token = Token('num', str(arg))
+            stack.add_token(token)
+            stack.enter()
+        if str(arg) in TOKENTYPES:
+            token = Token(arg)
+            stack.add_token(token)
+    for x in stack.stack:
+        if x != '':
+            print(x )
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ perly/consts.py
@@ -0,0 +1,10 @@
+DIGITS = "1234567890."
+
+BINARY_OPERATORS = '^+-*/'
+UNIARY_OPERATORS = 'n!'
+
+OPERATORS = "!^i+-*/n"
+FUNCTIONS = ['sin', 'cos', 'tan', 'asin', 'acos', 'atan', 'sq',
+             'sqrt', 'log', 'ln', 'pi', 'e']
+TRIG_FUNCS = ['sin', 'cos', 'tan', 'asin', 'acos', 'atan']
+TOKENTYPES = ['num', '+','-','*','/', 'n', '!','^', '.'] + FUNCTIONS
